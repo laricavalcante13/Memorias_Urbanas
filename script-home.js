c4c93936fcd6723f2f865fe326ea03d8a7f43323
lari.cavalcante <lari.cavalcante13@gmail.com>
2026-03-31 15:20:04 -0300

    dividing script.js

---
+++ script-home.js
@@ -0,0 +1,28 @@
+//Lógica do instascan em home.html para ler o QR Code e redirecionar para a página com o ID do monumento
+let scanner = new Instascan.Scanner({
+  video: document.getElementById('preview'),
+  mirror: false // Importante para ler QR Codes sem espelhar
+});
+
+scanner.addListener('scan', function (content) {
+  // O Instascan lê o conteúdo do QR Code. 
+  // Se o QR Code tiver apenas o ID (ex: "gilda"), redirecionamos:
+  window.location.href = 'index.html?id=' + content;
+});
+
+Instascan.Camera.getCameras().then(function (cameras) {
+  if (cameras.length > 0) {
+    // Tenta pegar a câmera traseira (geralmente a última da lista no Android/Chrome)
+    let selectedCam = cameras[cameras.length - 1];
+    scanner.start(selectedCam);
+  }
+  else {
+    alert('Nenhuma câmera encontrada. Verifique as permissões do navegador.');
+  }
+}).catch(function (e) {
+  console.error(e);
+  alert('Erro ao acessar câmera: ' + e);
+});
+
+
+
---
+++ script-lista.js
@@ -0,0 +1,30 @@
+//Lógica de lista.html para carregar a lista de monumentos do JSON
+
+const listaContainer = document.getElementById('lista-monumentos');
+
+// Carrega o JSON
+fetch('dados.json')
+  .then(res => res.json())
+  .then(dados => {
+    dados.forEach(monumento => {
+      // Cria o HTML de cada card
+      const card = document.createElement('a');
+      card.href = `index.html?id=${monumento.id}`;
+      card.className = 'card';
+
+      card.innerHTML = `
+                        <img src="${monumento.imagem}" class="card-img" alt="${monumento.nome_popular}">
+                        <div class="card-info">
+                            <h2>${monumento.nome_popular}</h2>
+                            <p>${monumento.categoria} • ${monumento.ano}</p>
+                        </div>
+                        <div class="seta">→</div>
+                    `;
+
+      listaContainer.appendChild(card);
+    });
+  })
+  .catch(err => {
+    console.error("Erro ao carregar lista:", err);
+    listaContainer.innerHTML = "<p>Erro ao carregar o acervo.</p>";
+  });
